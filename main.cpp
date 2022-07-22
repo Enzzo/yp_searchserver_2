b51e5f5f3bfa888b8f8293f40ffd408f5df7e394
Sergey <enzzomatrix@gmail.com>
2022-07-22 14:53:30 +1100

    added document id and relevance

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <map>
 
 using namespace std::literals;
 
@@ -20,7 +21,9 @@ int ReadLineWithNumber() {
     return result;
 }
 
-std::set<std::string> VectorToSet(const std::vector<std::string>&);
+std::set<std::string> VectorToSet(const std::vector<std::string>& vs) {
+    return { vs.begin(), vs.end() };
+}
 
 std::vector<std::string> SplitIntoWords(const std::string& text) {
     std::vector<std::string> words;
@@ -61,10 +64,14 @@ std::vector<std::string> SplitIntoWordsNoStop(const std::string& text, const std
     return words;
 }
 
-void AddDocument(std::vector<std::vector<std::string>>& documents, const std::set<std::string>& stop_words,
+void AddDocument(
+    std::vector<std::pair<int, std::vector<std::string>>>& documents, 
+    const std::set<std::string>& stop_words,
+    int document_id,
     const std::string& document) {
+    
     const std::vector<std::string> words = SplitIntoWordsNoStop(document, stop_words);
-    documents.push_back(words);
+    documents.push_back({ document_id, words });
 }
 
 std::set<std::string> ParseQuery(const std::string& text, const std::set<std::string>& stop_words) {
@@ -72,23 +79,35 @@ std::set<std::string> ParseQuery(const std::string& text, const std::set<std::st
     return query_words;
 }
 
-bool MatchDocument(const std::vector<std::string>& document_words, const std::set<std::string>& query_words) {
-    for (const std::string& word : VectorToSet(document_words)) {
+int MatchDocument(const std::pair<int, std::vector<std::string>>& content, 
+    const std::set<std::string>& query_words) {
+
+    int relevance = 0;
+
+    const auto& [id, doc] = content;
+
+    for (const std::string& word : VectorToSet(doc)) {
         std::set<std::string>::const_iterator w = query_words.find(word);
-        if (w != query_words.end()) return true;
+        if (w != query_words.end()) relevance++;
     }
-    return false;
+
+    return relevance;
 }
 
-std::vector<int> FindDocuments(const std::vector<std::vector<std::string>>& documents, const std::set<std::string>& stop_words,
+std::vector<std::pair<int, int>> FindDocuments(
+    const std::vector<std::pair<int, std::vector<std::string>>>& documents, 
+    const std::set<std::string>& stop_words,
     const std::string& query) {
-    std::vector<int> matched_documents;
 
-    for (size_t i = 0; i < documents.size(); ++i) {
-        if (MatchDocument(documents[i], ParseQuery(query, stop_words))) {
-            matched_documents.push_back(i);
+    const std::set<std::string> query_words = ParseQuery(query, stop_words);
+    std::vector<std::pair<int, int>> matched_documents;
+
+    for (const auto& [id, document] : documents) {
+        int relevance = MatchDocument({ id, document }, query_words);
+        if (relevance > 0) {
+            matched_documents.push_back({ id, relevance });
         }
-    }
+    }    
 
     return matched_documents;
 }
@@ -98,18 +117,14 @@ int main() {
     const std::set<std::string> stop_words = ParseStopWords(stop_words_joined);
 
     // Read documents
-    std::vector<std::vector<std::string>> documents;
+    std::vector<std::pair<int, std::vector<std::string>>> documents;
     const int document_count = ReadLineWithNumber();
     for (int document_id = 0; document_id < document_count; ++document_id) {
-        AddDocument(documents, stop_words, ReadLine());
+        AddDocument(documents, stop_words, document_id, ReadLine());
     }
 
     const std::string query = ReadLine();
-    for (const int document_id : FindDocuments(documents, stop_words, query)) {
-        std::cout << "{ document_id = "s << document_id << " }"s << std::endl;
+    for (const auto& [id, relevance] : FindDocuments(documents, stop_words, query)) {
+        std::cout << "{ document_id = "s << id << ", relevance = "s<<relevance<<" }"s << std::endl;
     }
-}
-
-std::set<std::string> VectorToSet(const std::vector<std::string>& v) {
-    return { v.begin(), v.end() };
 }
